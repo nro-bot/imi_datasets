db8cecc113be1187b8686860cd0fe9c16c595445
Diony Rosa <dhrosa@gmail.com>
2023-03-24 15:39:59 -0400

    Basic multi-threading support in ocr.py

--- LegalDataset/florida/ocr.py
+++ LegalDataset/florida/ocr.py
@@ -33,31 +33,49 @@ def pdf_to_text(pdf_path, text_path):
 
 def main():
     import argparse
+    from concurrent.futures import ThreadPoolExecutor
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--input_dir",
         "-i",
-        required=True,
         type=pathlib.Path,
+        required=True,
         help="Directory containing input PDF files.",
     )
     parser.add_argument(
         "--output_dir",
         "-o",
-        required=True,
         type=pathlib.Path,
+        required=True,
         help="Directory to output OCR'd text files to. The generated filenames will have the same structure as the input filenames.",
+    )
+    parser.add_argument(
+        "--threads",
+        "-t",
+        type=int,
+        default=8,
+        help="Number of PDFs to process in parallel.",
     )
     args = parser.parse_args()
 
     output_dir = args.output_dir
     output_dir.mkdir(exist_ok=True)
-    for pdf_path in tqdm(list(args.input_dir.glob("*.pdf"))):
+
+    pdf_paths = list(args.input_dir.glob("*.pdf"))
+
+    def process(pdf_path):
         text_path = output_dir / f"{pdf_path.stem}.txt"
         if text_path.exists():
-            continue
+            return
         pdf_to_text(pdf_path, text_path)
+
+    with (
+        ThreadPoolExecutor(max_workers=args.threads) as executor,
+        tqdm(total=len(pdf_paths)) as progress,
+    ):
+        for _ in executor.map(process, pdf_paths):
+            progress.update()
 
 
 if __name__ == "__main__":

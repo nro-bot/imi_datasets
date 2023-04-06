6ad34d9cc931072c148ae22721acc52b652492f5
N Ouyang <nouyang@alum.mit.edu>
2023-04-06 17:07:08 -0400

    updated wisconsin scripts

--- LegalDataset/wisconsin/download_pdfs.py
+++ LegalDataset/wisconsin/download_pdfs.py
@@ -1,4 +1,13 @@
-# make list
+# 06 April 2023
+# nrobot
+
+# Script to automatically (currently I pulled with a GUI tool) download all PDFs
+# in massage category from Wisconsin site
+
+# For ethical reasons, want to avoid sharing entire set of PDFs
+# Instead provide script to replicate dataset.
+
+
 import requests
 from bs4 import BeautifulSoup
 from pathlib import Path

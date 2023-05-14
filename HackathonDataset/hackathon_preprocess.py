2bf1aa4a058df15ac0b4863041bd4b747de04f90
N Ouyang <nouyang@alum.mit.edu>
2023-05-14 18:17:27 -0400

    committed code to cfreate dataset for hackathon

--- HackathonDataset/hackathon_preprocess.py
+++ HackathonDataset/hackathon_preprocess.py
@@ -1,8 +1,24 @@
 import pandas as pd
 from pandas import DataFrame, Series
 
+def export_for_reading(infile='nogit_data/list_of_post_contents.csv'): 
+    raw = pd.read_csv(infile)
+    readable = ['time_downloaded', 'posted_date_readable', 'src_category_name','thread_page_url', 'author', 'author_url', 'post_text' ]
+    df[readable].to_csv('nogit_data/post_data_only.csv', index=False)
+    #posts = pd.read_csv('nogit_data/hackathon_ripgrep_post_contents.csv')
+    N = 100
+    df.sample(n=N, seed=42).to_csv(f'nogit_data/{N}_sample_posts.csv')
+
+def extract_sample():
+    print('hi') 
+
 
 def clean(df):
+    # -- Clean of usernames
+    # -- Clean of names
+    # -- Clean addresses from reviews
+    # -- Clean of 
+
     import re
     import base64
     from pandas.util import hash_pandas_object

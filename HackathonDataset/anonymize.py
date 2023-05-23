dd4b320262e719d4bc445ad90c996b928dcc240b
N Ouyang <nouyang@alum.mit.edu>
2023-05-23 14:22:57 -0400

    skeletonized anonymizeization

--- HackathonDataset/anonymize.py
+++ HackathonDataset/anonymize.py
@@ -1,24 +1,18 @@
 import pandas as pd
 from pandas import DataFrame, Series
+import subprocess
 
-def export_for_reading(infile='nogit_data/list_of_post_contents.csv'): 
-    raw = pd.read_csv(infile)
-    readable = ['time_downloaded', 'posted_date_readable', 'src_category_name','thread_page_url', 'author', 'author_url', 'post_text' ]
-    df[readable].to_csv('nogit_data/post_data_only.csv', index=False)
-    #posts = pd.read_csv('nogit_data/hackathon_ripgrep_post_contents.csv')
-    N = 100
-    df.sample(n=N, seed=42).to_csv(f'nogit_data/{N}_sample_posts.csv')
+#p = subprocess.run(
+        #['git', 'rev-parse', '--show-toplevel'], capture_output=True)
+#GIT_HOME = p.stdout.decode('utf-8').strip()
 
-def extract_sample():
-    print('hi') 
+# -- Three anonymizations
+# Phone numbers
+# Usernames
+# Addresses
+# Names?
 
-
-def clean(df):
-    # -- Clean of usernames
-    # -- Clean of names
-    # -- Clean addresses from reviews
-    # -- Clean of 
-
+def remove_usernames():
     import re
     import base64
     from pandas.util import hash_pandas_object
@@ -32,18 +26,38 @@ def clean(df):
     author_ids = {a: base64.b64encode(a.to_bytes(8, 'big')) for a in hash_pandas_object(all_authors)}
     print(author_ids)
 
+def remove_addresses():
+    pass
+
+def remove_phones():
+    pass
+
+def export_for_reading(infile='nogit_data/post_data_only.csv'): 
+    pass
+
+def extract_sample():
+    pass
+
+
+def clean(df):
+    pass
 
 def main():
+    pass
+
+main()
+
+
+'''
     import argparse
     import ast
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--input', help='Input filename for raw forum posts CSV file.', type=argparse.FileType('r'), default='posts_raw.csv')
+    parser.add_argument('-i', '--input', help='Input filename for raw forum posts CSV file.', type=argparse.FileType('r'), default='list_of_post_contents.csv')
     parser.add_argument('-n', '--num_rows', help='Number of rows to read from input file.', type=int)
 
     args = parser.parse_args()
     raw = pd.read_csv(args.input, nrows=args.num_rows)
 
     clean(raw)
-
-main()
+'''
--- HackathonDataset/fix_dtypes_for_hackathon.py
+++ HackathonDataset/fix_dtypes_for_hackathon.py
@@ -15,56 +15,46 @@ def comma_to_int(df, col_name):
     df[col_name] = df[col_name].astype(int)
     return df
 
-filepath = Path('nogit_data', 'list_of_post_contents.csv')
-df = pd.read_csv(filepath)
 
-# -- Basic cleaning
-df = df.dropna(subset='time_downloaded') # drop (my hacky) comment rows 
-df = df.drop_duplicates('post_id') # post_id is unique across entire forum
-# -- Export
-df.to_csv('nogit_data/list_of_post_contents_nona.csv', index=False)
+def clean_column_types():
+    filepath = Path('nogit_data', 'list_of_post_contents.csv')
+    df = pd.read_csv(filepath)
 
-# -- Convert relevant columns to datetime objects 
-# df.join_date_data.parallel_apply(lambda x: pd.to_datetime(x, unit = 's')) # ignore this column, it conflicts with the human readable date for unkown reason
-df.join_date_readable.parallel_apply(lambda x: pd.to_datetime(x))
-# -- Export
+    # -- Drop N/As and duplicates and unused columns
+    df = df.dropna(subset='time_downloaded') # drop (my hacky) comment rows 
+    df = df.drop_duplicates('post_id') # post_id is unique across entire forum
+    df.to_csv('nogit_data/list_of_post_contents_nona.csv', index=False)
 
-# -- Turn columns with commas, into integers
-df = pd.read_csv('nogit_data/list_of_post_contents_nona.csv')
-for column in ['post_ordinal', 'author_num_posts', 'thread_page_num', 'thread_max_pages', 'num_quotes']:
-    print(column)
-    df = comma_to_int(df, column)
+    df.drop(columns=['comment', 'join_date_data', 'posted_date_data'], inplace=True)
+    df.drop(columns=['num_likers'], inplace=True)
 
+    # -- Turns strings into datetime type
+    # use join_date_readable, not join_date_data; the two columns don't quite match up 
+    # df.join_date_data.parallel_apply(lambda x: pd.to_datetime(x, unit = 's'))
+    df.join_date_readable.parallel_apply(lambda x: pd.to_datetime(x))
 
+    # -- Turn strings into integers
+    df = pd.read_csv('nogit_data/list_of_post_contents_nona.csv')
+    for column in ['post_ordinal', 'author_num_posts', 'thread_page_num', 'thread_max_pages', 'num_quotes']:
+        print(column)
+        df = comma_to_int(df, column)
 
-df = pd.read_csv('nogit_data/list_of_post_contents_nona_nocomma.csv')
-# -- Drop some columns we don't need
-df.drop(columns=['comment', 'join_date_data', 'posted_date_data'], inplace=True)
-# -- Export
-df.to_csv('nogit_data/list_of_post_contents_nona_nocomma_nodate.csv', index=False)
-# -- Drop this columns too
-df.drop(columns=['num_likers'], inplace=True)
-# -- Export
-df.to_csv('nogit_data/list_of_post_contents_nona_nocomma_nodate_nonumlikers.csv', index=False)
+    # -- Remove mixed type from post_text by replacing NaNs with strings 
+    f['post_text'] = df['post_text'].fillna('')#, inplace=True)
 
-pd.read_csv('nogit_data/list_of_post_contents_nona_nocomma_nodate_nonumlikers.csv')
+    def debug_types():
+        # -- Check column types 
+        with pd.option_context('display.max_rows', 100, 'display.max_columns', 30):
+            display(df[df.duplicated(subset=['post_ordinal', 'post_id', 'post_text'])])
+        # DtypeWarning: Columns (3) have mixed types. Specify dtype option on import or set low_memory=False.
+        for col in df.columns:
+            unique_types = df[col].apply(type).unique()
+            if len(unique_types) > 1: # if a column is mixed type
+                print(col, unique_types)
 
+    # -- Export finally
+    readable = ['author', 'posted_date_readable', 'src_category_name', 'thread_page_url', 'post_ordinal', 'post_text']
+    df[readable].to_csv('nogit_data/post_data_only.csv', index=False)
+    df.to_csv('nogit_data/dtype_fixed_posts_df.pkl')
 
-# check for duplicates
-with pd.option_context('display.max_rows', 100, 'display.max_columns', 30):
-    display(df[df.duplicated(subset=['post_ordinal', 'post_id', 'post_text'])])
-
-# -- Following code helpful for debugging
-# DtypeWarning: Columns (3) have mixed types. Specify dtype option on import or set low_memory=False.
-
-for col in df.columns:
-    unique_types = df[col].apply(type).unique()
-    if len(unique_types) > 1: # if a column is mixed type
-        print(col, unique_types)
-
-# -- Remove mixed type (float) from post_text column
-f['post_text'] = df['post_text'].fillna('')#, inplace=True)
-
-# -- Export finally
-readable = ['author', 'posted_date_readable', 'src_category_name', 'thread_page_url', 'post_ordinal', 'post_text']
-df[readable].to_csv('nogit_data/post_data_only.csv', index=False)
+clean_column_types()
